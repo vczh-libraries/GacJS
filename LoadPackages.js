0a771d3c40635ae5e5c9a78838fb853e1515dd05
vczh <vczh@163.com>
2015-05-20 01:38:05 -0700

    ...

---
+++ LoadPackages.js
@@ -0,0 +1,23 @@
+var AllJavaScriptPackages = [
+    "./Script/Package.js",
+    "./Script/Test.js",
+    "./Script/Html/Events.js",
+    "./Script/Html/RunAfterWindowLoaded.js",
+    "./Script/Html/ResizeEvent.js",
+    "./Script/Html/Navigation.js",
+    "./Script/GacUI/Layout/Basic.js",
+    "./Script/GacUI/Layout/Stack.js",
+    "./Script/GacUI/Layout/Table.js",
+    "./Script/GacUI/Layout/Misc.js",
+    "./Script/GacUI/Elements/Interface.js",
+    "./Script/GacUI/Elements/Basic.js",
+    "./Script/GacUI/Elements/ColorizedText.js",
+    "./Script/GacUI/Elements/Document.js",
+    "./Script/GacUI/Types.js",
+    "./Script/Class.js",
+];
+
+for (var i = 0; i < AllJavaScriptPackages.length; i++) {
+    var currentJavaScriptPackage = JSON.stringify(AllJavaScriptPackages[i]);
+    document.write("<script src=" + currentJavaScriptPackage + "></script>");
+}
---
+++ Script/Html/Navigation.js
@@ -0,0 +1,3 @@
+Packages.Define("Html.Navigation", ["Class"], function (__injection__) {
+    eval(__injection__);
+});
