f77149b746306a2a2ca84499a5587dccef990c4e
Zihan Chen <vczh@163.com>
2015-06-05 02:20:52 -0700

    Doc/Delay.js: WaitAll, WaitAny

--- Doc/Delay.js
+++ Doc/Delay.js
@@ -18,6 +18,10 @@ API:
         Future FailedThen(function generator);
         Future Then(function generator);
     }
+
+    {promise:Promise, future:Future} CreateDelay();
+    Future<object[]> WaitAll(Future[]);
+    Future<{index:number, result:object}> WaitAny(Future[]);
 */
 Packages.Define("Doc.Delay", ["Class"], function (__injection__) {
     eval(__injection__);
@@ -52,6 +56,10 @@ Packages.Define("Doc.Delay", ["Class"], function (__injection__) {
             }
         }),
         Result: Public.Property({}),
+
+        HasResult: Public(function () {
+            return this.callbacks === null;
+        }),
 
         DelayExecute: Public(function (callback) {
             if (this.callbacks === null) {
@@ -104,6 +112,10 @@ Packages.Define("Doc.Delay", ["Class"], function (__injection__) {
                 return this.delay.Result;
             }),
             Result: Public.Property({ readonly: true }),
+
+            HasResult: Public(function () {
+                return this.delay.HasResult();
+            }),
 
             SucceededThen: Public.StrongTyped(Future, [__Function], function (generator) {
                 var delay = new Delay();
@@ -166,10 +178,74 @@ Packages.Define("Doc.Delay", ["Class"], function (__injection__) {
         }
     }
 
+    function WaitAll(futures) {
+        var count = futures.length;
+        var finished = 0;
+        var delay = CreateDelay();
+
+        function OnReady() {
+            var result = futures.map(function (future) {
+                return future.Result;
+            });
+            delay.promise.SetResult(result);
+        }
+
+        for (var i = 0; i < count; i++) {
+            var future = futures[i];
+            if (future.HasResult()) {
+                finished++;
+            }
+            else {
+                future.Then(function (value) {
+                    finished++;
+                    if (finished === count) {
+                        OnReady();
+                    }
+                });
+            }
+        }
+
+        if (finished === count) {
+            OnReady();
+        }
+        return delay.future;
+    }
+
+    function WaitAny(futures) {
+        var count = futures.length;
+        var delay = CreateDelay();
+
+        function OnReady(index) {
+            var result = { index: index, result: futures[index].Result };
+            delay.promise.SetResult(result);
+        }
+
+        not_ready: {
+            for (var i = 0; i < count; i++) {
+                if (futures[i].HasResult()) {
+                    OnReady(i);
+                    break not_ready;
+                }
+                else (function () {
+                    var index = i;
+                    futures[i].Then(function (value) {
+                        if (!delay.future.HasResult()) {
+                            OnReady(index);
+                        }
+                    })
+                })();
+            }
+        }
+
+        return delay.future;
+    }
+
     return {
         DelayException: DelayException,
         Promise: Promise,
         Future: Future,
         CreateDelay: CreateDelay,
+        WaitAll: WaitAll,
+        WaitAny: WaitAny,
     }
 })
