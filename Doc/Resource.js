c63f213c5c417f43efd094a6af5140ec2c5c81a9
Zihan Chen <vczh@163.com>
2015-06-05 19:35:12 -0700

    Doc/Resource.js API

--- Doc/Resource.js
+++ Doc/Resource.js
@@ -1,10 +1,43 @@
 /*
 API:
-    
+    interface IResourceDeserializer
+    {
+        string Name { get; }
+        string PriorDeserializerName { get; }
+        object Deserialize(object resource);
+    }
+
+    void RegisterStaticResource(fileNamePattern, deserializer);
+    Future GetStaticResourceAsync(path);
 */
 Packages.Define("Doc.Resource", ["Class", "Doc.Delay", "Doc.Wildcard"], function (__injection__) {
     eval(__injection__);
 
+    var IResourceDeserializer = Class("<Doc.Resource>::IResourceDeserializer", {
+        GetName: Public.Abstract(),
+        Name: Public.Property({ readonly: true }),
+
+        GetPriorDeserializerName: Public.Virtual(function () {
+            return null;
+        }),
+        PriorDeserializerName: Public.Property({ readonly: true }),
+
+        Deserialize: Public.Virtual(function (resource) {
+            return resource;
+        }),
+    });
+
+    function RegisterStaticResource(fileNamePattern, deserializer) {
+
+    }
+
+    function GetStaticResourceAsync(path) {
+
+    }
+
     return {
+        IResourceDeserializer: IResourceDeserializer,
+        RegisterStaticResource: RegisterStaticResource,
+        GetStaticResourceAsync: GetStaticResourceAsync,
     }
 })
