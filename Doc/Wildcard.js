fb81f741e8da778f7ee7c15cafdeccd438ae2895
Zihan Chen <vczh@163.com>
2015-06-05 19:24:48 -0700

    Doc/Wildcard.js

--- Doc/Wildcard.js
+++ Doc/Wildcard.js
@@ -1,15 +1,37 @@
 /*
 API:
-    Regex WildcardToRegex(string wildcard);
+    RegExp WildcardToRegExp(string wildcard);
 */
 Packages.Define("Doc.Wildcard", function (__injection__) {
     eval(__injection__);
 
-    function WildcardToRegex(wildcard) {
-
+    function WildcardToRegExp(wildcard) {
+        var regex = "";
+        for (var i = 0; i < wildcard.length; i++) {
+            var c = wildcard[i];
+            switch (c) {
+                case "*":
+                    regex += ".*";
+                    break;
+                case "?":
+                    regex += ".";
+                    break;
+                case "(": case ")":
+                case "[": case "]":
+                case "{": case "}":
+                case "*": case "+": case "?":
+                case "$": case "^":
+                case "\\": case "/":
+                    regex += "\\" + c;
+                    break;
+                default:
+                    regex += c;
+            }
+        }
+        return new RegExp(regex);
     }
 
     return {
-        WildcardToRegex: WildcardToRegex,
+        WildcardToRegExp: WildcardToRegExp,
     }
 })
