37639137551ae417ad280181d210b4bc42009a36
vczh <vczh@163.com>
2015-06-22 04:40:31 -0700

    Document Draft

--- Doc/Document.js
+++ Doc/Document.js
@@ -1,5 +1,29 @@
-Packages.Define("Doc.Document", ["Class", "Doc.Resource"], function (__injection__) {
+Packages.Define("Doc.Document", ["Class", "Html.Razor", "IO.Resource", "IO.Wildcard"], function (__injection__) {
     eval(__injection__);
+
+    /********************************************************************************
+    RazorResourceDeserializer
+    ********************************************************************************/
+
+    var RazorResourceDeserializer = Class(PQN("RazorResourceDeserializer"), IResourceDeserializer, {
+        GetName: Public.Override(function () {
+            return "Razor";
+        }),
+
+        GetPriorDeserializerName: Public.Override(function () {
+            return "Text";
+        }),
+
+        Deserialize: Public.Override(function (resource) {
+            return CompileRazor(resource, "Html.MVCRazorHelper");
+        }),
+    });
+    RegisterDeserializer(new RazorResourceDeserializer());
+    RegisterResource(WildcardToRegExp("*.razor.html"), "Razor");
+
+    /********************************************************************************
+    Package
+    ********************************************************************************/
 
     return {
     }
--- Doc/TreeView.js
+++ Doc/TreeView.js
@@ -1,6 +1,49 @@
-Packages.Define("Doc.TreeView", ["Class", "Doc.Resource", "Doc.Document"], function (__injection__) {
+Packages.Define("Doc.TreeView", ["Class", "IO.Resource", "Doc.Document"], function (__injection__) {
     eval(__injection__);
 
+    function Dom(type, parent) {
+        var node = document.createElement(type);
+        if (parent) {
+            parent.appendChild(node);
+        }
+        return node;
+    }
+
+    /********************************************************************************
+    TreeNodeContainer
+    ********************************************************************************/
+
+    var TreeNodeContainer = Class(PQN("TreeNodeContainer"), {
+        element: Private(null),
+        visible: Private(false),
+
+        GetElement: Private(function () { return this.element; }),
+        Element: Public.Property({ readonly: true }),
+
+        GetVisible: Private(function () { return this.visible; }),
+        SetVisible: Private.StrongTyped(__Void, [__Boolean], function (value) {
+            this.visible = value;
+            if (value) {
+                this.element.setAttribute("class", "TreeNodeContainer Expanded");
+            }
+            else {
+                this.element.setAttribute("class", "TreeNodeContainer Collapsed");
+            }
+        }),
+        Visible: Public.Property({}),
+
+        __Constructor: Public.StrongTyped(__Void, [__Boolean, __Object], function (visible, parent) {
+            this.element = Dom("ul", parent);
+            this.element.doc_Owner = this.__ExternalReference;
+            this.Visible = visible;
+        }),
+    });
+
+    /********************************************************************************
+    Package
+    ********************************************************************************/
+
     return {
+        TreeNodeContainer: TreeNodeContainer,
     }
 })
