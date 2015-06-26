fc0c9da95c8fa081907a01239120061897e2b291
vczh <vczh@163.com>
2015-06-26 01:56:36 -0700

    ...

--- Doc/Document.js
+++ Doc/Document.js
@@ -118,6 +118,78 @@ Packages.Define("Doc.Document", ["Class", "Html.Razor", "IO.Resource", "IO.Wildc
     RegisterResource(WildcardToRegExp("ns(*).xml"), "Ns");
 
     /********************************************************************************
+    TreeResourceDeserializer
+    ********************************************************************************/
+
+    var DocSymbolTreeItem = Class(PQN("DocSymbolTreeItem"), {
+        Tags: Public(null),
+        Key: Public(""),
+        DisplayName: Public(""),
+        Doc: Public(false),
+        ChildNames: Public(null),
+        Children: Public(null),
+    });
+
+    var TreeResourceDeserializer = Class(PQN("TreeResourceDeserializer"), IResourceDeserializer, {
+        GetName: Public.Override(function () {
+            return "Tree";
+        }),
+
+        GetPriorDeserializerName: Public.Override(function () {
+            return "Xml";
+        }),
+
+        CreateSymbol: Private(function (symbolXml) {
+            var item = new DocSymbolTreeItem();
+            item.Tags = symbolXml.getAttribute("Tags").split(";");
+            item.Key = symbolXml.getAttribute("Key");
+            item.DisplayName = symbolXml.getAttribute("DisplayName");
+            item.Doc = symbolXml.getAttribute("Doc") === true;
+            item.ChildNames = [];
+            item.Children = {};
+
+            for (var i = 0; i < symbolXml.childNodes.length; i++) {
+                var childXml = symbolXml.childNodes[i];
+                switch (childXml.tagName) {
+                    case "Symbol":
+                        var childItem = this.CreateSymbol(childXml);
+                        item.ChildNames.push(childItem.DisplayName);
+                        item.Children[childItem.DisplayName] = [childItem];
+                        break;
+                    case "Overloads":
+                        var displayName = childXml.getAttribute("DisplayName");
+                        item.ChildNames.push(displayName);
+                        var overloadItems = [];
+                        item.Children[displayName] = overloadItems;
+
+                        var childSymbolXmls = GetDirectXmlChild(childXml, "Symbol");
+                        for (var j = 0; j < childSymbolXmls.length; j++) {
+                            var childSymbolXml = childSymbolXmls[j];
+                            overloadItems.push(this.CreateSymbol(childSymbolXml));
+                        }
+                        break;
+                }
+            }
+
+            return item;
+        }),
+
+        Deserialize: Public.Override(function (resource) {
+            var parentMapping = {};
+            var mapXmls = GetDirectXmlChild(GetDirectXmlChild(resource.firstChild, "SymbolParentMapping")[0], "Map");
+            for (var i = 0; i < mapXmls.length; i++) {
+                var mapXml = mapXmls[i];
+                parentMapping[mapXml.getAttribute("From")] = mapXml.getAttribute("To");
+            }
+
+            var rootItem = this.CreateSymbol(GetDirectXmlChild(resource.firstChild, "Symbol")[0]);
+            return { parentMapping: parentMapping, rootItem: rootItem };
+        }),
+    });
+    RegisterDeserializer(new TreeResourceDeserializer());
+    RegisterResource(WildcardToRegExp("t(*).xml"), "Tree");
+
+    /********************************************************************************
     Package
     ********************************************************************************/
 
