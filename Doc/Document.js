337f0a981b952dbc600391ce7592056df4485436
vczh <vczh@163.com>
2015-06-26 01:23:24 -0700

    GacDN: NssResourceDeserializer

--- Doc/Document.js
+++ Doc/Document.js
@@ -22,6 +22,47 @@ Packages.Define("Doc.Document", ["Class", "Html.Razor", "IO.Resource", "IO.Wildc
     RegisterResource(WildcardToRegExp("*.razor.html"), "Razor");
 
     /********************************************************************************
+    NssResourceDeserializer
+    ********************************************************************************/
+
+    var DocNamespaceItem = Struct(PQN("DocNamespaceItem"), {
+        Key: "",
+        DisplayName: "",
+        UrlName: "",
+        Doc: false,
+    });
+
+    var NssResourceDeserializer = Class(PQN("NssResourceDeserializer"), IResourceDeserializer, {
+        GetName: Public.Override(function () {
+            return "Nss";
+        }),
+
+        GetPriorDeserializerName: Public.Override(function () {
+            return "Xml";
+        }),
+
+        Deserialize: Public.Override(function (resource) {
+            var nsXmls = resource.getElementsByTagName("Namespace");
+            var list = [];
+            var map = {};
+            for (var i = 0; i < nsXmls.length; i++) {
+                var nsXml = nsXmls[i];
+                var item = new DocNamespaceItem(
+                    nsXml.getAttribute("Key"),
+                    nsXml.getAttribute("DisplayName"),
+                    nsXml.getAttribute("UrlName"),
+                    nsXml.getAttribute("Doc") === "true"
+                    );
+                list.push(item);
+                map[item.Key] = item;
+            }
+            return { list: list, map: map };
+        }),
+    });
+    RegisterDeserializer(new NssResourceDeserializer());
+    RegisterResource(WildcardToRegExp("nss.xml"), "Nss");
+
+    /********************************************************************************
     Package
     ********************************************************************************/
 
