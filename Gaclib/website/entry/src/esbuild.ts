642a623a7b6d8f1cf9a8a3b125f2ae9c8ee7cc71
vczh <vczh@outlook.com>
2025-08-11 14:37:49 -0700

    ...

--- Gaclib/website/entry/src/esbuild.ts
+++ Gaclib/website/entry/src/esbuild.ts
@@ -1,10 +1,15 @@
 import { build } from 'esbuild';
 import * as path from 'path';
 
+const args = process.argv.slice(2);
+const isShip = args.includes('--ship');
+
 await build({
     entryPoints: [path.resolve(`./lib/index.js`)],
     bundle: true,
-    minify: true,
+    minify: isShip,
+    sourcemap: !isShip,
+    keepNames: !isShip,
     format: 'iife',
     globalName: 'GacUIHtmlRenderer',
     outfile: path.resolve(`./lib/dist/index.js`),
