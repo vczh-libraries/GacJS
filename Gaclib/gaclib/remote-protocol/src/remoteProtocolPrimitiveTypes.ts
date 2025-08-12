136420f8b4ec522aab0762ab60b1b4c3d056a0e3
vczh <vczh@outlook.com>
2025-08-12 06:06:45 -0700

    Update type definitions and add test DOM data
    Modified remote protocol primitive types to allow null for List, ArrayMap, and Dictionary types. Added sample RenderingDom JSON objects to TestDomDiff.ts for future testing purposes.

--- Gaclib/gaclib/remote-protocol/src/remoteProtocolPrimitiveTypes.ts
+++ Gaclib/gaclib/remote-protocol/src/remoteProtocolPrimitiveTypes.ts
@@ -9,9 +9,9 @@ export type Binary = string; // base64 encoded
 
 export type Ptr<T> = T | null;
 export type Nullable<T> = T | null;
-export type List<T> = T[];
-export type ArrayMap<T, Key extends string> = T extends Record<Key, {}> ? T[] : never;
-export type Dictionary<K, T> = [[K, T]];
+export type List<T> = T[] | null;
+export type ArrayMap<T, Key extends string> = (T extends Record<Key, {}> ? T[] : never) | null;
+export type Dictionary<K, T> = [[K, T]] | null;
 
 // GacUI\Source\GuiTypes.h
 export enum Key {
@@ -241,7 +241,7 @@ export enum Key {
     /*
      * Additionals
      */
-/* eslint-disable @typescript-eslint/no-duplicate-enum-values */
+    /* eslint-disable @typescript-eslint/no-duplicate-enum-values */
     KEY_OEM_1 = 0xBA,               /* ';:' for US */
     KEY_OEM_2 = 0xBF,               /* '/?' for US */
     KEY_OEM_3 = 0xC0,               /* '`~' for US */
@@ -250,5 +250,5 @@ export enum Key {
     KEY_OEM_6 = 0xDD,               /* ']}' for US */
     KEY_OEM_7 = 0xDE,               /* ''"' for US */
     KEY_HANJA = 0x19,
-/* eslint-enable @typescript-eslint/no-duplicate-enum-values */
+    /* eslint-enable @typescript-eslint/no-duplicate-enum-values */
 }
--- Gaclib/gaclib/renderer/test/TestDomDiff.ts
+++ Gaclib/gaclib/renderer/test/TestDomDiff.ts
@@ -1,4 +1,75 @@
+import * as SCHEMA from '@gaclib/remote-protocol';
 import { assert, test } from 'vitest';
+
+// @ts-expect-error TS6133
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const inputDomJsonSingleRoot: SCHEMA.RenderingDom = {
+    id: -1,
+    content: {
+        hitTestResult: null,
+        cursor: null,
+        element: null,
+        bounds: { x1: 0, y1: 1, x2: 2, y2: 3 },
+        validArea: { x1: 4, y1: 5, x2: 6, y2: 7 }
+    },
+    children: null
+};
+
+// @ts-expect-error TS6133
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const inputDomJsonBinaryTree: SCHEMA.RenderingDom = {
+    id: -1,
+    content: {
+        hitTestResult: null,
+        cursor: null,
+        element: null,
+        bounds: { x1: 0, y1: 1, x2: 2, y2: 3 },
+        validArea: { x1: 4, y1: 5, x2: 6, y2: 7 }
+    },
+    children: [{
+        id: 0,
+        content: {
+            hitTestResult: SCHEMA.WindowHitTestResult.Client,
+            cursor: null,
+            element: null,
+            bounds: { x1: 0, y1: 0, x2: 0, y2: 0 },
+            validArea: { x1: 0, y1: 0, x2: 0, y2: 0 }
+        },
+        children: [{
+            id: 2,
+            content: {
+                hitTestResult: null,
+                cursor: SCHEMA.WindowSystemCursorType.Arrow,
+                element: null,
+                bounds: { x1: 2, y1: 2, x2: 2, y2: 2 },
+                validArea: { x1: 2, y1: 2, x2: 2, y2: 2 }
+            },
+            children: null
+        },
+        {
+            id: 3,
+            content: {
+                hitTestResult: SCHEMA.WindowHitTestResult.Title,
+                cursor: SCHEMA.WindowSystemCursorType.Help,
+                element: null,
+                bounds: { x1: 3, y1: 3, x2: 3, y2: 3 },
+                validArea: { x1: 3, y1: 3, x2: 3, y2: 3 }
+            },
+            children: null
+        }]
+    },
+    {
+        id: 1,
+        content: {
+            hitTestResult: null,
+            cursor: null,
+            element: 99,
+            bounds: { x1: 1, y1: 1, x2: 1, y2: 1 },
+            validArea: { x1: 1, y1: 1, x2: 1, y2: 1 }
+        },
+        children: null
+    }]
+};
 
 test(`EmptyTest`, () => {
     assert.isTrue(true);
