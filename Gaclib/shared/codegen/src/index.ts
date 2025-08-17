4620b21d615c9c410a8a17aa308cc008f82da3fa
vczh <vczh@outlook.com>
2025-08-17 15:09:13 -0700

    ...

--- Gaclib/shared/codegen/src/index.ts
+++ Gaclib/shared/codegen/src/index.ts
@@ -1,8 +1,8 @@
 import { generateRemoteProtocol } from './remote-protocol/generateRemoteProtocol.js';
 import { generateRemoteProtocolInvoking } from './remote-protocol/generateRemoteProtocolInvoking.js';
-// import { generateSnapshotIndex, prepareSnapshots } from './snapshots.js';
+import { generateSnapshotIndex, prepareSnapshots } from './snapshots.js';
 
 generateRemoteProtocol();
 generateRemoteProtocolInvoking();
-// prepareSnapshots();
-// generateSnapshotIndex();
+prepareSnapshots();
+generateSnapshotIndex();
